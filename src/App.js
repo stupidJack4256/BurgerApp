7168df8c0f24d3b1557e6dcf5a007c2ea51c8d71
root <anilmj4256@gmail.com>
2020-03-04 12:15:34 +0000

    first day commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-   		<h1>HI Anil</h1>
-		  <h2>Welcome to Burger App</h2>
-    </div>
-  );
+class App extends Component {
+	render() {
+	
+		return (
+			<div>
+					<Layout>
+						<BurgerBuilder/>
+					</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
