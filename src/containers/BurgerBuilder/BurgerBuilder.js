a6c7419074f7f747acf5b3bf4a7debf8c9335fb8
root <anilmj4256@gmail.com>
2020-03-09 09:54:27 +0000

    commit after adding a post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -81,7 +82,25 @@ class BurgerBuilder extends Component {
 		this.setState({purchasing:false})
 	}
 	purchaseContinueHandler=()=>{
-		alert("Continue!")
+		// alert("Continue!")
+		const order ={
+			ingredients: this.state.ingredients,
+			price 	   : this.state.totalPrice,
+			customer   : {
+				name: 'Anil MJ',
+				address : {
+					street: 'Jack street',
+					zipcode: '425642',
+					country: 'India'
+				},
+			email     : 'mjanil425@ymail.com'
+			},
+			deliveryMethod: 'fastest'
+		}
+		axios.post('/orders.json', order)
+		.then(response => console.log(response))
+		.catch(error => console.log(error));
+		
 	}
 	
 	render() {
